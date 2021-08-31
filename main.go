4c97abcfdb34db08e7ebc11210bd29685c29b078
Adrien Delorme <azr@users.noreply.github.com>
2021-08-31 15:25:44 +0200

    consolidate version location

--- main.go
+++ main.go
@@ -9,23 +9,9 @@ import (
 	dockerpush "github.com/hashicorp/packer-plugin-docker/post-processor/docker-push"
 	dockersave "github.com/hashicorp/packer-plugin-docker/post-processor/docker-save"
 	dockertag "github.com/hashicorp/packer-plugin-docker/post-processor/docker-tag"
+	"github.com/hashicorp/packer-plugin-docker/version"
 
 	"github.com/hashicorp/packer-plugin-sdk/plugin"
-	"github.com/hashicorp/packer-plugin-sdk/version"
-)
-
-var (
-	// Version is the main version number that is being run at the moment.
-	Version = "1.0.2"
-
-	// VersionPrerelease is A pre-release marker for the Version. If this is ""
-	// (empty string) then it means that it is a final release. Otherwise, this
-	// is a pre-release such as "dev" (in development), "beta", "rc1", etc.
-	VersionPrerelease = "dev"
-
-	// PluginVersion is used by the plugin set to allow Packer to recognize
-	// what version this plugin is.
-	PluginVersion = version.InitializePluginVersion(Version, VersionPrerelease)
 )
 
 func main() {
@@ -35,7 +21,7 @@ func main() {
 	pps.RegisterPostProcessor("push", new(dockerpush.PostProcessor))
 	pps.RegisterPostProcessor("save", new(dockersave.PostProcessor))
 	pps.RegisterPostProcessor("tag", new(dockertag.PostProcessor))
-	pps.SetVersion(PluginVersion)
+	pps.SetVersion(version.PluginVersion)
 	err := pps.Run()
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err.Error())
